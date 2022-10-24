8510e52f9c90876c65f34b38c370f6c125e95a05
Laurence Rawlings <laurence.rawlings@solirius.com>
2022-10-24 18:33:09 +0100

    feat: redesign landing page

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -8,22 +8,24 @@ import SecondaryButton from '@/Components/SecondaryButton.vue';
 <template>
     <Head title="Welcome" />
 
-    <div class="flex flex-col items-top justify-center min-h-screen bg-green-400 sm:items-center sm:pt-0">
-        <ApplicationMark class="w-16 h-16 mb-2" />
-        <div class="text-2xl text-green-600 mb-2">
-            Welcome to <span class="font-black">Libbit</span>
-        </div>
-        <div class="text-green-600 mb-6 text-lg">
-            Save solutions to common problems with the libraries you use
-        </div>
-        <div>
-            <Link href="/login">
-                <PrimaryButton>Login</PrimaryButton>
-            </Link>
-            <span class="mx-2">or</span>
-            <Link href="/register">
-                <SecondaryButton>Register</SecondaryButton>
-            </Link>
+    <div class="grid place-items-center min-h-screen bg-gradient-to-br from-green-200 via-green-300 to-green-400 sm:pt-0">
+        <div class="grid place-items-center bg-white shadow-xl p-8 sm:p-16 sm:rounded-lg">
+            <ApplicationMark class="w-16 h-16 mb-2" />
+            <div class="text-2xl text-green-600 mb-2 text-center">
+                Welcome to <span class="font-black">Libbit</span>
+            </div>
+            <div class="text-green-600 mb-6 text-lg text-center">
+                Save solutions to common problems with the libraries you use!
+            </div>
+            <div>
+                <Link href="/login">
+                    <PrimaryButton>Login</PrimaryButton>
+                </Link>
+                <span class="mx-4">or</span>
+                <Link href="/register">
+                    <SecondaryButton>Register</SecondaryButton>
+                </Link>
+            </div>
         </div>
     </div>
 </template>
